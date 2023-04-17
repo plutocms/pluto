5e7308bd937c9918afc2e8b4855675ec67656ada
Victor Ribeiro <devjvribeiro@gmail.com>
2023-04-17 14:18:11 -0300

    chore: updated Vulmix to 0.6.0-beta

---
+++ vulmix.config.ts
@@ -0,0 +1,13 @@
+import type { VulmixConfig } from "vulmix/types/vulmix-config";
+
+module.exports = <VulmixConfig>{
+  lang: 'pt-BR',
+  head: {
+    meta: [
+      {
+        name: 'title',
+        content: 'Pluto CMS',
+      }
+    ]
+  }
+};
