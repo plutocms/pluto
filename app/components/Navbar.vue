da82129d03268af0f72603aad4eb2ceff7902af8
Victor Ribeiro <devjvribeiro@gmail.com>
2025-03-27 19:53:40 -0300

    feat: Navbar

---
+++ app/components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav class="sticky top-0 z-50 h-14 bg-white dark:bg-black">
+    <div class="flex h-full items-center justify-between px-4">
+      <ColorModeButton />
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts"></script>
