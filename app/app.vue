504e90076387cc04bc78a1c8df632e2b093bdac7
Victor Ribeiro <devjvribeiro@gmail.com>
2025-03-27 19:55:58 -0300

    feat: entry point view

--- app/app.vue
+++ app/app.vue
@@ -2,10 +2,41 @@
   <div class="h-full">
     <UApp>
       <NuxtRouteAnnouncer />
-      <NuxtPage />
+      <NuxtLoadingIndicator />
+
+      <Navbar />
+
+      <div class="flex h-full items-stretch">
+        <div class="w-[300px] shrink-0 bg-white/5 px-4 py-8">
+          <UNavigationMenu
+            :items="menu"
+            orientation="vertical"
+            class="data-[orientation=vertical]:w-full"
+          />
+        </div>
+
+        <div class="grow overflow-hidden bg-white/10">
+          <NuxtPage />
+        </div>
+      </div>
     </UApp>
   </div>
 </template>
+
+<script setup lang="ts">
+  import type { NavigationMenuItem } from '@nuxt/ui'
+
+  const menu = ref<NavigationMenuItem[]>([
+    {
+      label: 'Home',
+      href: '/admin',
+    },
+    {
+      label: 'Products',
+      href: '/admin/products',
+    },
+  ])
+</script>
 
 <style>
   html,
