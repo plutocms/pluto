71bd86718b380e6dbe7fc1dfae1e21402e7f82f9
Victor Ribeiro <devjvribeiro@gmail.com>
2025-03-27 18:00:51 -0300

    feat: added product endpoints

---
+++ server/api/product/[slug].ts
@@ -0,0 +1,22 @@
+import { serverSupabaseClient } from '#supabase/server'
+import type { Database } from '~~/types/supabase'
+
+export default defineEventHandler(async event => {
+  const params = event.context.params
+  const client = await serverSupabaseClient<Database>(event)
+
+  if (params) {
+    const { data, error } = await client
+      .from('products')
+      .select('*')
+      .eq('slug', params?.slug)
+      .limit(1)
+      .single()
+
+    if (error) {
+      throw createError({ statusMessage: error.message })
+    }
+
+    return { data }
+  }
+})
---
+++ server/api/products.ts
@@ -0,0 +1,14 @@
+import { serverSupabaseClient } from '#supabase/server'
+import type { Database } from '~~/types/supabase'
+
+export default defineEventHandler(async event => {
+  const client = await serverSupabaseClient<Database>(event)
+
+  const { data, error } = await client.from('products').select('*')
+
+  if (error) {
+    throw createError({ statusMessage: error.message })
+  }
+
+  return { data }
+})
