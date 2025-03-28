bb9ae5155dcb9bca2ea14a60414e55da8d4b0ffd
Victor Ribeiro <devjvribeiro@gmail.com>
2025-03-28 03:48:35 -0300

    fix(Navbar): colors

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -1,5 +1,7 @@
 <template>
-  <nav class="sticky top-0 z-50 h-14 bg-white dark:bg-black">
+  <nav
+    class="sticky top-0 z-50 h-10 border-b border-black/10 bg-black/90 text-white dark:border-white/5 dark:bg-black"
+  >
     <div class="flex h-full items-center justify-between px-4">
       <ColorModeButton />
     </div>
