1d451d17744563af6657170f288d5b244121e746
Victor Ribeiro <devjvribeiro@gmail.com>
2025-03-29 00:54:27 -0300

    feat: `useObjectURL` composable

---
+++ app/composables/object-url.ts
@@ -0,0 +1,9 @@
+export function useObjectURL(file: File) {
+  const blob = shallowRef(file ? URL.createObjectURL(file) : undefined)
+
+  watch(toRef(file), value => {
+    blob.value = value ? URL.createObjectURL(value) : undefined
+  })
+
+  return { blob }
+}
