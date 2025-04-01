f26d713050af8eea4a869e603d624f4185338ad1
Victor Ribeiro <devjvribeiro@gmail.com>
2025-03-31 21:07:03 -0300

    feat: added supabase typegen script

---
+++ scripts/supabase-typegen.ts
@@ -0,0 +1,5 @@
+import { $ } from 'bun'
+
+const projectId = process.env.SUPABASE_PROJECT_ID
+
+await $`npx -y supabase gen types typescript --project-id "${projectId}" --schema public > types/supabase.ts`
