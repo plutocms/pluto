f87141069f7037c069bbceaeebfeb14205affd62
Victor Ribeiro <devjvribeiro@gmail.com>
2025-04-01 02:28:39 -0300

    feat: media selection

--- app/components/UploadMedia.vue
+++ app/components/UploadMedia.vue
@@ -8,6 +8,18 @@
           <template #gallery>
             <div class="flex flex-col gap-y-6 pt-6">
               <div class="flex items-center justify-between">
+                <div>
+                  <span v-if="selectedMedia.length === 0">
+                    Showing {{ mediaList?.data.length }} file{{
+                      mediaList?.data.length === 1 ? '' : 's'
+                    }}
+                  </span>
+
+                  <span v-else>
+                    {{ selectedMedia.length }} of
+                    {{ mediaList?.data.length }} files selected
+                  </span>
+                </div>
 
                 <div>
                   <UButton
@@ -20,15 +32,27 @@
                 </div>
               </div>
 
-              <div class="grid grid-cols-5 gap-2">
+              <div class="grid grid-cols-5 gap-3">
                 <div
                   v-for="file in mediaList?.data"
                   :key="file.id"
-                  class="aspect-square overflow-hidden rounded-2xl bg-black/20"
+                  class="group/image-select relative aspect-square overflow-hidden rounded-2xl bg-black select-none hover:ring-1 has-[[aria-checked='true']]:ring-2 has-[[aria-checked='true']]:ring-green-400"
                 >
+                  <label
+                    class="absolute top-0 left-0 grid h-full w-full items-start justify-end"
+                  >
+                    <div class="rounded-bl-2xl bg-black/80 p-2">
+                      <UCheckbox
+                        :model-value="isSelected(file.id)"
+                        @change="selectMedia(file.id)"
+                      />
+                    </div>
+                  </label>
+
                   <img
+                    v-if="file.name"
                     :src="getMediaUrl(file.name)"
-                    :alt="file.name"
+                    :alt="file.alt || file.name"
                     class="h-full w-full object-contain"
                   />
                 </div>
@@ -214,5 +238,23 @@
     mediaBlobList.value = []
 
     reset()
+  }
+
+  const selectedMedia = ref<number[]>([])
+
+  function isSelected(id: number) {
+    return selectedMedia.value.includes(id)
+  }
+
+  function selectMedia(id: number) {
+    if (selectedMedia.value.includes(id)) {
+      const index = selectedMedia.value.indexOf(id)
+
+      selectedMedia.value.splice(index, 1)
+
+      return
+    }
+
+    selectedMedia.value.push(id)
   }
 </script>
