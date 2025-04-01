5cf12a32748f9ac3ed06c612837c9f4a8223e959
Victor Ribeiro <devjvribeiro@gmail.com>
2025-04-01 02:31:51 -0300

    fix(API): get media from table instead of bucket

--- server/api/media/list.ts
+++ server/api/media/list.ts
@@ -4,19 +4,13 @@ import type { Database } from '~~/types/supabase'
 export default defineEventHandler(async event => {
   const client = await serverSupabaseClient<Database>(event)
 
-  const { data, error } = await client.storage.from('media').list('uploads', {
-    limit: 100,
-    offset: 0,
-    sortBy: { column: 'name', order: 'asc' },
-  })
+  const { data, error } = await client.from('media').select('*')
 
   if (error) {
-    createError({
+    throw createError({
       message: 'error',
     })
-  } else {
-    return {
-      data,
-    }
   }
+
+  return { data }
 })
