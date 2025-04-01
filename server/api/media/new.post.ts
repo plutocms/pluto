161e72e17a297afff930955acb339f387dfddc90
Victor Ribeiro <devjvribeiro@gmail.com>
2025-04-01 02:33:26 -0300

    chore: renamed variables

--- server/api/media/new.post.ts
+++ server/api/media/new.post.ts
@@ -5,22 +5,21 @@ import { kebabCase } from 'change-case'
 export default defineEventHandler(async event => {
   const client = await serverSupabaseClient<Database>(event)
 
-  const file = await readMultipartFormData(event)
+  const formData = await readMultipartFormData(event)
 
   const year = new Date().getFullYear().toString()
   const month = Intl.NumberFormat('en-US', {
     minimumIntegerDigits: 2,
   }).format(new Date().getMonth() + 1)
 
-  if (!file || !file?.[0].filename)
+  if (!formData || !formData?.[0].filename)
     throw createError({
       message: 'No file sent.',
     })
 
-  const fileName = file[0].filename.replace(/(.*)(\.\w+$)/, `$1`)
-  const fileExtension = file[0].filename.replace(/(.*)(\.\w+$)/, `$2`)
+  const fileName = formData[0].filename.replace(/(.*)(\.\w+$)/, `$1`)
+  const fileExtension = formData[0].filename.replace(/(.*)(\.\w+$)/, `$2`)
 
-  const { data, error } = await client.storage
     .from('media')
     .upload(
       `uploads/${year}-${month}-${kebabCase(fileName)}${fileExtension}`,
@@ -30,9 +29,11 @@ export default defineEventHandler(async event => {
         upsert: true,
       }
     )
+  const { data: mediaUploadData, error: mediaUploadError } =
+    await client.storage
 
-  if (error) {
     createError({
+  if (mediaUploadError) {
       message: 'error',
     })
   } else {
