b574795c1d189e434c0cba5ae0a73d601ee138e9
Victor Ribeiro <devjvribeiro@gmail.com>
2025-04-08 17:27:27 -0300

    fix: make `getMediaUrl` accept null or undefined

--- app/composables/media.ts
+++ app/composables/media.ts
@@ -1,7 +1,9 @@
 export function useMedia() {
   const config = useRuntimeConfig()
 
-  function getMediaUrl(fileName: string): string {
+  function getMediaUrl(fileName: string | null | undefined): string {
+    if (!fileName) return ''
+
     return `${config.public.supabase.url}/storage/v1/object/public/media/uploads/${fileName}`
   }
 
