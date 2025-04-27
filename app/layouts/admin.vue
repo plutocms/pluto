7fc27785e1af3f127d45dd6d9d083e295c4a91f1
Victor Ribeiro <devjvribeiro@gmail.com>
2025-04-27 02:39:41 -0300

    feat: make sidebar links better

--- app/layouts/admin.vue
+++ app/layouts/admin.vue
@@ -3,11 +3,15 @@
     <NavbarAdmin />
 
     <div class="flex h-full items-stretch">
-      <div class="bg-admin-sidebar w-64 shrink-0 px-4 py-8">
+      <div class="bg-admin-sidebar w-64 shrink-0 px-4 py-4">
         <UNavigationMenu
           :items="menu"
           orientation="vertical"
           class="data-[orientation=vertical]:w-full"
+          :ui="{
+            link: 'text-base gap-x-3 font-normal',
+          }"
+          highlight
         />
       </div>
 
@@ -25,14 +29,31 @@
     {
       label: 'Home',
       href: '/admin/home',
+      icon: 'lucide:house',
     },
     {
       label: 'Products',
       href: '/admin/products',
+      icon: 'lucide:box',
+      onSelect: () => {
+        navigateTo('/admin/products')
+      },
+      children: [
+        {
+          label: 'Create new product',
+          href: '/admin/product/new',
+        },
+
+        {
+          label: 'All products',
+          href: '/admin/products',
+        },
+      ],
     },
     {
       label: 'Media',
       href: '/admin/media',
+      icon: 'lucide:images',
     },
   ])
 </script>
