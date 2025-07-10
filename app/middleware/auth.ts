27f745789dd61c29610636ababc55adf124b4fab
Victor Ribeiro <devjvribeiro@gmail.com>
2025-07-10 03:18:55 -0300

    fix: prevent navigating to login/signup if logged-in

---
+++ app/middleware/auth.ts
@@ -0,0 +1,14 @@
+export default defineNuxtRouteMiddleware((to) => {
+  const user = useSupabaseUser()
+
+  if (
+    user.value &&
+    (to.path === '/admin/login' || to.path === '/admin/signup')
+  ) {
+    return navigateTo('/admin/home')
+  }
+
+  if (!user.value && to.path !== '/admin/login') {
+    return navigateTo('/admin/login')
+  }
+})
