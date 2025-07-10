d68f90c0880288784ee8500f5f0b5419c81c94c8
Victor Ribeiro <devjvribeiro@gmail.com>
2025-07-10 00:51:22 -0300

    feat: created Sign Up page

--- app/pages/admin/(auth)/signup.vue
+++ app/pages/admin/(auth)/signup.vue
@@ -4,20 +4,28 @@ definePageMeta({
 })
 
 useHead({
-  title: 'Login',
+  title: 'Sign Up',
 })
 
 const supabase = useSupabaseClient()
 
 const form = ref({
   email: '',
+  first_name: '',
+  last_name: '',
   password: '',
 })
 
 async function submitForm() {
-  await supabase.auth.signInWithPassword({
+  await supabase.auth.signUp({
     email: form.value.email,
     password: form.value.password,
+    options: {
+      data: {
+        first_name: form.value.first_name,
+        last_name: form.value.last_name,
+      },
+    },
   })
 
   navigateTo('/admin')
@@ -29,15 +37,34 @@ async function submitForm() {
     <UCard class="w-[400px]">
       <form @submit.prevent="submitForm">
         <div class="flex flex-col gap-y-6">
-          <h1>Login</h1>
+          <h1>Create Account</h1>
 
           <UFormField>
             <UInput
               v-model="form.email"
+              type="email"
               placeholder="victor@example.com"
               class="w-full"
             />
           </UFormField>
+
+          <div class="flex gap-x-4">
+            <UFormField>
+              <UInput
+                v-model="form.first_name"
+                placeholder="e.g: John"
+                class="w-full"
+              />
+            </UFormField>
+
+            <UFormField>
+              <UInput
+                v-model="form.last_name"
+                placeholder="e.g: Doe"
+                class="w-full"
+              />
+            </UFormField>
+          </div>
 
           <UFormField>
             <UInput
@@ -50,13 +77,18 @@ async function submitForm() {
 
           <div class="flex items-center justify-between gap-x-4">
             <UFormField>
-              <UButton type="submit"> Login </UButton>
+              <UButton
+                to="/admin/login"
+                icon="lucide:arrow-left"
+                variant="link"
+                class="px-0"
+              >
+                Back to Login
+              </UButton>
             </UFormField>
 
             <UFormField>
-              <UButton variant="link" to="/admin/signup" class="px-0">
-                Create Account
-              </UButton>
+              <UButton type="submit"> Create account </UButton>
             </UFormField>
           </div>
         </div>
