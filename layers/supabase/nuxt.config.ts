1d987469aab6bd5cb6a68836498e143095c5e12b
Victor Ribeiro <devjvribeiro@gmail.com>
2025-08-18 10:27:25 -0300

    chore: added supabase runtime configs

--- layers/supabase/nuxt.config.ts
+++ layers/supabase/nuxt.config.ts
@@ -5,6 +5,11 @@ export default defineNuxtConfig({
     name: 'supabase',
   },
 
+  runtimeConfig: {
+    supabaseUrl: process.env.SUPABASE_URL,
+    supabaseKey: process.env.SUPABASE_KEY,
+  },
+
   ignore: ['./scripts/**', './supabase/**'],
 
   supabase: {
