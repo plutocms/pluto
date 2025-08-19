7aece3909ad9a3f5ede551e448ed347827f3ad13
Victor Ribeiro <devjvribeiro@gmail.com>
2025-08-19 01:09:09 -0300

    fix: added `tresjs/cientos` to optimized dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,6 +23,7 @@ export default defineNuxtConfig({
           'change-case',
           'yup',
           '@vueuse/integrations/useChangeCase',
+          '@tresjs/cientos',
         ],
       },
     },
