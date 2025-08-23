36e5828a8fe5a6baf71ca0e37aa1c412b0835bcf
Victor Ribeiro <devjvribeiro@gmail.com>
2025-08-22 21:41:06 -0300

    fix: color mode button size

--- app/components/ColorModeButton.vue
+++ app/components/ColorModeButton.vue
@@ -17,6 +17,7 @@ const isDark = computed({
       :icon="isDark ? 'i-lucide-moon' : 'i-lucide-sun'"
       color="neutral"
       variant="solid"
+      size="sm"
       @click="isDark = !isDark"
     />
 
