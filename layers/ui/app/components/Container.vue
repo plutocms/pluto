5e03a2d6310f355e31da6eb473fc00ab434c1f33
Victor Ribeiro <devjvribeiro@outlook.com>
2025-09-12 13:11:17 -0300

    feat(ui): Container component

---
+++ layers/ui/app/components/Container.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { ClassNameValue } from 'tailwind-merge'
+import { twMerge } from 'tailwind-merge'
+
+const props = defineProps<{
+  class?: ClassNameValue
+}>()
+
+const resolvedClasses = computed(() => {
+  const defaultClasses =
+    'mx-auto max-w-full transition-all px-4 sm:px-6 w-[1400px]'
+
+  return twMerge([defaultClasses, props.class])
+})
+</script>
+
+<template>
+  <div :class="resolvedClasses">
+    <slot />
+  </div>
+</template>
