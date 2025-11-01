aa0e5b4846797b6a3f088d52907d527334aa8d86
Victor Ribeiro <devjvribeiro@gmail.com>
2025-11-01 00:48:43 -0300

    chore: moved `product` API routes to `supabase-shop` layer

---
+++ layers/supabase-shop/shared/types/product.ts
@@ -0,0 +1,8 @@
+export type Media = SupabaseDatabase['public']['Tables']['media']['Row']
+export type Product = SupabaseDatabase['public']['Tables']['products']['Row']
+
+export interface ProductWithMedia extends Product {
+  media: Media[]
+}
+
+export type CartItem = ProductWithMedia
