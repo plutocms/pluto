1b798539a240a63fe30b0818fb51799b7821bda6
Victor Ribeiro <devjvribeiro@gmail.com>
2025-11-09 03:39:02 -0300

    chore: removed unused interface

--- layers/supabase-shop/shared/types/product.ts
+++ layers/supabase-shop/shared/types/product.ts
@@ -1,8 +1,4 @@
 export type Media = SupabaseDatabase['public']['Tables']['media']['Row']
 export type Product = SupabaseDatabase['public']['Tables']['products']['Row']
 
-export interface ProductWithMedia extends Product {
-  media: Media[]
-}
-
 export type CartItem = ProductItem
