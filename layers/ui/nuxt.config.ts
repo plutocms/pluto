ca5feaec3474ac7d5a91ba9235bce2edba5154c9
Victor Ribeiro <devjvribeiro@gmail.com>
2025-11-21 00:48:43 -0300

    chore(deps): added tailwindcss package instead of Nuxt module

--- layers/ui/nuxt.config.ts
+++ layers/ui/nuxt.config.ts
@@ -1,7 +1,13 @@
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   modules: ['reka-ui/nuxt'],
 
   $meta: {
     name: 'ui',
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 })
