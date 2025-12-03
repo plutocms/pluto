4d251be568508dee91f3f041a5a3423e72d27ec8
Victor Ribeiro <devjvribeiro@gmail.com>
2025-12-02 21:37:07 -0300

    chore: moved supabase-shop to its own repo

--- layers/supabase-shop/app/plugins/navbar-admin-actions.ts
+++
@@ -1,7 +0,0 @@
-import NavbarAdminActions from '../components/NavbarAdminActions.vue'
-
-export default defineNuxtPlugin(() => {
-  const { addAction } = useNavbarAdminActions()
-
-  addAction(markRaw(NavbarAdminActions))
-})
--- layers/supabase-shop/shared/types/product.ts
+++
@@ -1,4 +0,0 @@
-export type Media = SupabaseDatabase['public']['Tables']['media']['Row']
-export type Product = SupabaseDatabase['public']['Tables']['products']['Row']
-
-export type CartItem = ProductItem
