17f51edec48a86c0a14d860736af82173da02f76
Victor Ribeiro <devjvribeiro@gmail.com>
2025-12-22 13:12:02 -0300

    feat: add grid layout with placeholder cards to admin home

--- layers/ui/app/pages/admin/home.vue
+++ layers/ui/app/pages/admin/home.vue
@@ -8,6 +8,14 @@ useHead({
   <main>
     <AdminView>
       <h1 class="text-4xl font-bold">Admin Dashboard</h1>
+
+      <div class="grid grid-cols-3 gap-x-4 gap-y-6">
+        <div
+          v-for="i in 6"
+          :key="i"
+          class="h-[200px] rounded-3xl border border-gray-500"
+        />
+      </div>
     </AdminView>
   </main>
 </template>
