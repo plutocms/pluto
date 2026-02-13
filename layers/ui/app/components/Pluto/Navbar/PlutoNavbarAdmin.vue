134c856ad4c450b2aabe2aa6520007c3af42a6df
Victor Ribeiro <devjvribeiro@gmail.com>
2026-02-13 12:19:03 -0300

    feat(NavbarAdmin): make it a wrapper

---
+++ layers/ui/app/components/Pluto/Navbar/PlutoNavbarAdmin.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav
+    class="light:bg-white light:border-black/10 font-outfit sticky top-0 z-50 h-10 shrink-0 border-b dark:border-white/5 dark:bg-black dark:text-white"
+  >
+    <slot />
+  </nav>
+</template>
